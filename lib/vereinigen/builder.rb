ce58ae979987266a65474805156b74cfab160b94
Daniel Alejo <yourmediasoft@gmail.com>
2017-07-31 11:12:24 -0500

    Updating Dem Vereinigen

--- lib/vereinigen/builder.rb
+++ lib/vereinigen/builder.rb
@@ -1,10 +1,8 @@
 module Vereinigen
 	class Builder
 		def superslug(title)
-			slug = title.downcase.strip.gsub(/['`]/,"").gsub(/\s*@\s*/, " at ").gsub(/\s*&\s*/, " and ").gsub(/\s*[^A-Za-z0-9\.\-]\s*/, '-').gsub(/_+/,"-").gsub(' ', '-').gsub(/[^\w-]/, '') 
+			slug = title.downcase.strip.gsub(/['`]/, '').gsub(/\s*@\s*/, " at ").gsub(/\s*&\s*/, " and ").gsub(/\s*[^A-Za-z0-9\.\-]\s*/, '-').gsub(/_+/, '-').gsub(' ', '-').gsub(/[^\w-]/, '') 
         	puts slug
 		end
 	end
 end
-
-
